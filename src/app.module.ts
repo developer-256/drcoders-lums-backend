89d9a385c64345d33a770fca6962784cf91450b7
developer-256 <ch256.it@gmail.com>
2025-02-01 12:11:49 +0500

    added typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DB_URL,
+      // host: 'localhost',
+      // port: 5432,
+      // username: 'postgres',
+      // password: 'Hello1122',
+      // database: 't3crud_drizzle',
+      ssl: true,
+      entities: [],
+      synchronize: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
